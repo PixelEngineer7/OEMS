4e2a043b9a4f42815ba27a21eae9c4a5be4f2d70
Gitendrajeet Ramlochund <61977663+PixelEngineer7@users.noreply.github.com>
2024-11-25 15:51:50 +0400

    Add files via upload
    Added the Web Application files

---
+++ components/navbar.php
@@ -0,0 +1,36 @@
+<nav id="sidebar">
+    <div class="sidebar_blog_1">
+        <div class="sidebar-header">
+            <div class="logo_section">
+                <a href="dashboard.php"><img class="logo_icon img-responsive" src="../../assets/logo/logo_icon.png" alt="#" /></a>
+            </div>
+        </div>
+        <div class="sidebar_user_info">
+            <div class="icon_setting"></div>
+            <div class="user_profle_side">
+                <div class="user_img"><img class="img-responsive" src="../../assets/images/admin_img.gif" alt="#" /></div>
+                <div class="user_info">
+                    <h6>Administrator</h6>
+                    <p><span class="online_animation"></span> Online</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sidebar_blog_2">
+        <h4 style="text-align:center">Employee Management System</h4>
+        <ul class="list-unstyled components">
+            <li><a href="mainDashboard.php"><i class="fa-solid fa-gauge-high green_color"></i> <span>Dashboard</span></a></li>
+            <li><a href="informationDashboard.php"><i class="fa-solid fa-info orange_color"></i> <span>News Management</span></a></li>
+            <li> <a href="userCreationDashboard.php"><i class="fa fa-user-plus blue1_color"></i> <span>User Management</span></a></li>
+            <li><a href="pmsDashboard.php"><i class="fa fa-bar-chart-o green_color"></i> <span>PMS Management</span></a></li>
+            <li><a href="leaveDashboard.php"><i class="fa fa-calendar-times-o red_color"></i> <span>Leaves Management</span></a></li>
+            <li><a href="payslipDashboard.php"><i class="fa fa-money blue1_color" aria-hidden="true"></i> <span>E-Payslip Management</span></a></li>
+            <li><a href="attendanceDashboard.php"><i class="fa fa-check-circle yellow_color"></i> <span>Daily Attendance</span></a></li>
+            <li><a href="taskDashboard.php"><i class="fa fa-tasks purple_color2"></i> <span>Task Management</span></a></li>
+            <li><a href="backup_restore.php"><i class="fa fa-database green_color"></i> <span>Backup | Restore DB</span></a></li>
+            <li><a href="reports.php"><i class="fa fa-file-text-o blue1_color"></i> <span>Reports</span></a></li>
+            <li><a href="company_directory.php"><i class="fa-solid fa-address-book brown_color"></i> <span>Company Directory</span></a></li>
+
+        </ul>
+    </div>
+</nav>
